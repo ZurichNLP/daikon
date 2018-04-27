29b35802c32466713bd703b37660ef1e3ca7d426
Mathias Müller <mathias.mueller@uzh.ch>
2018-04-27 16:11:23 +0200

    improve translation

--- daikon/translate.py
+++ daikon/translate.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import io
 import os
 import sys
 import logging
@@ -17,50 +18,95 @@ from daikon import constants as C
 def softmax(x):
     return np.exp(x) / np.sum(np.exp(x), axis=0)
 
-
-def translate(load_from: str, input_text: str = [], train_mode: bool = False, **kwargs):
-    """Generates a translation by predicting from a trained translation model. See argument
-    description in `bin/daikon`."""
+def load_vocabs(load_from: str):
 
     source_vocab = vocab.Vocabulary()
     source_vocab.load(os.path.join(load_from, C.SOURCE_VOCAB_FILENAME))
     target_vocab = vocab.Vocabulary()
     target_vocab.load(os.path.join(load_from, C.TARGET_VOCAB_FILENAME))
 
+    return source_vocab, target_vocab
+
+def translate_line(session,
+                   line,
+                   source_vocab,
+                   target_vocab,
+                   encoder_inputs,
+                   decoder_inputs,
+                   decoder_targets,
+                   decoder_logits):
+    """
+    TODO
+    """
+    source_ids = np.array(source_vocab.get_ids(line.split())).reshape(1, -1)
+
+    translated_ids = []
+
+    for _ in range(C.TRANSLATION_MAX_LEN):
+
+        # target ids will serve as decoder inputs and decoder targets,
+        # but decoder targets will not be used to compute logits
+        target_ids = np.array([C.BOS_ID] + translated_ids).reshape(1, -1)
+
+        feed_dict = {encoder_inputs: source_ids,
+                     decoder_inputs: target_ids,
+                     decoder_targets: target_ids}
+        logits_result = session.run([decoder_logits], feed_dict=feed_dict)
+
+        # first session result, first item in batch, target symbol at last position
+        next_symbol_logits = logits_result[0][0][-1]
+        next_id = np.argmax(next_symbol_logits)
+
+        if next_id in [C.EOS_ID, C.PAD_ID]:
+            break
+
+        translated_ids.append(next_id)
+
+    words = target_vocab.get_words(translated_ids)
+
+    return ' '.join(words)
+
+def translate_lines(load_from: str, input_lines: List[str], train_mode: bool = False, **kwargs):
+    """
+    TODO
+    """
+    source_vocab, target_vocab = load_vocabs(load_from)
+
     # fix batch_size to 1
     encoder_inputs, decoder_targets, decoder_inputs, _, _, decoder_logits, _ = compgraph.define_computation_graph(source_vocab.size, target_vocab.size, 1)
 
     saver = tf.train.Saver()
-
-    source_ids = np.array(source_vocab.get_ids(input_text.split())).reshape(1, -1)
 
     with tf.Session() as session:
 
         # load model
         saver.restore(session, os.path.join(load_from, C.MODEL_FILENAME))
 
-        translated_sequence = []
+        translations = []
 
-        # TODO: increase permissible length of translation?
-        for _ in range(C.MAX_LEN):
+        for line in input_file_handle:
+            translation = translate_line(session, line, source_vocab, target_vocab, encoder_inputs, decoder_inputs, decoder_targets, decoder_logits)
+            translations.append(translation)
 
-            # target ids will serve as decoder inputs and decoder targets,
-            # but decoder targets will not be used to compute logits
-            target_ids = np.array([C.BOS_ID] + translated_sequence).reshape(1, -1)
+    return translations
 
-            feed_dict = {encoder_inputs: source_ids,
-                         decoder_inputs: target_ids,
-                         decoder_targets: target_ids}
-            logits_result = session.run([decoder_logits], feed_dict=feed_dict)
 
-            # first session result, first item in batch, target symbol at last position
-            next_symbol_logits = logits_result[0][0][-1]
-            next_symbol = np.argmax(next_symbol_logits)
+def translate_file(load_from: str, input_file_handle: io.TextIOWrapper, output_file_handle: io.TextIOWrapper, **kwargs):
+    """
+    TODO
+    """
+    source_vocab, target_vocab = load_vocabs(load_from)
 
-            if next_symbol in [C.EOS_ID, C.PAD_ID]:
-                break
+    # fix batch_size to 1
+    encoder_inputs, decoder_targets, decoder_inputs, _, _, decoder_logits, _ = compgraph.define_computation_graph(source_vocab.size, target_vocab.size, 1)
 
-            translated_sequence.append(next_symbol)
+    saver = tf.train.Saver()
 
-    words = target_vocab.get_words(translated_sequence)
-    return ' '.join(words)
+    with tf.Session() as session:
+
+        # load model
+        saver.restore(session, os.path.join(load_from, C.MODEL_FILENAME))
+
+        for line in input_file_handle:
+            translation = translate_line(session, line, source_vocab, target_vocab, encoder_inputs, decoder_inputs, decoder_targets, decoder_logits)
+            output_file_handle.write(translation + "\n")
